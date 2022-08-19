f254585ac4145ee9af34796fd271d1dadf4fc425
BananaFlamber <korolevsteel@inbox.ru>
2022-08-19 16:21:54 +0300

    little update

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ int number2 = int.Parse(Console.ReadLine());
 
 if  (number1 > number2)
 {
-    Console.WriteLine($"Большее число {number1}");
+    Console.WriteLine($"Большее число: {number1}");
 }
 else
 {
-    Console.WriteLine($"Большее число {number2}");
+    Console.WriteLine($"Большее число: {number2}");
 }
